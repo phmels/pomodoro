f0398d9ad78107dbcc4352cc1effd2cee4b675c0
phmels <phmels@gmail.com>
2022-04-25 20:57:04 +0100

    pomodoro final

---
+++ lib/componentes/EntradaTempo.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:pomodoro/store/pomodoro.store.dart';
+import 'package:provider/provider.dart';
+
+class EntradaTempo extends StatelessWidget {
+  final String titulo;
+  final int valor;
+  final void Function()? inc;
+  final void Function()? dec;
+
+  const EntradaTempo({
+    Key? key,
+    required this.titulo,
+    required this.valor,
+    this.inc,
+    this.dec,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final store = Provider.of<PomodoroStore>(context);
+    return Observer(
+      builder: (_) {
+        return Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              titulo,
+              style: const TextStyle(
+                fontSize: 25,
+              ),
+            ),
+            const SizedBox(height: 10),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: dec,
+                  child: const Icon(
+                    Icons.arrow_downward,
+                    color: Colors.white,
+                  ),
+                  style: ElevatedButton.styleFrom(
+                    shape: const CircleBorder(),
+                    padding: const EdgeInsets.all(15),
+                    primary:
+                        store.estaTrabalhando() ? Colors.red : Colors.green,
+                  ),
+                ),
+                Text(
+                  '${this.valor} min',
+                  style: const TextStyle(
+                    fontSize: 18,
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: this.inc,
+                  child: const Icon(
+                    Icons.arrow_upward,
+                    color: Colors.white,
+                  ),
+                  style: ElevatedButton.styleFrom(
+                    shape: const CircleBorder(),
+                    padding: const EdgeInsets.all(15),
+                    primary:
+                        store.estaTrabalhando() ? Colors.red : Colors.green,
+                  ),
+                ),
+              ],
+            ),
+          ],
+        );
+      },
+    );
+  }
+}
